d815974bde866a3ad4829067f873a3fed13f4694
Ivan Gagis <igagis@gmail.com>
2023-01-24 16:48:29 +0200

    stuff

--- src/pautina/http_server.hpp
+++ src/pautina/http_server.hpp
@@ -3,15 +3,32 @@
 #include <string>
 #include <functional>
 
+#include <setka/tcp_server_socket.hpp>
+
+#include <nitki/queue.hpp>
+
 namespace pautina{
 
 class http_server{
+    setka::tcp_server_socket accept_socket;
+
+    nitki::queue queue;
+
+    opros::wait_set wait_set;
+
+    volatile bool quit_flag = false;
 public:
+    http_server();
+    ~http_server();
 
     void add(
         std::vector<std::string> path,
         std::function<void()> handler
     );
+
+    void run();
+
+    void quit();
 };
 
 }
