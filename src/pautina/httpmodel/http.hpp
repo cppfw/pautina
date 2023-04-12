53fc40aad132c59b084c961f3cb2703b8aa28107
Ivan Gagis <igagis@gmail.com>
2023-04-12 09:20:36 +0300

    stuff

--- src/pautina/httpmodel/http.hpp
+++ src/pautina/httpmodel/http.hpp
@@ -86,12 +86,69 @@ std::string_view method_to_string(method m) noexcept;
  * @brief HTTP response status code.
  */
 enum class status_code {
+	http_100, // Continue
+	http_101, // Switching Protocols
+	http_102, // Processing
+	http_103, // Early Hints
 	http_200, // OK
 	http_201, // Created
-	// TODO: add all status codes
+	http_202, // Accepted
+	http_203, // Non-Authoritative Information
+	http_204, // No Content
+	http_205, // Reset Content
+	http_206, // Partial Content
+	http_207, // Multi-Status
+	http_208, // Already Reported
+	http_226, // IM Used
+	http_300, // Multiple Choices
+	http_301, // Moved Permanently
+	http_302, // Found
+	http_303, // See Other
+	http_304, // Not Modified
+	http_305, // Use Proxy
+	http_306, // Switch Proxy
+	http_307, // Temporary Redirect
+	http_308, // Permanent Redirect
+	http_400, // Bad Request
+	http_401, // Unauthorized
+	http_402, // Payment Required
+	http_403, // Forbidden
 	http_404, // Not Found
-
-	// TODO: add all status codes
+	http_405, // Method Not Allowed
+	http_406, // Not Acceptable
+	http_407, // Proxy Authentication Required
+	http_408, // Request Timeout
+	http_409, // Conflict
+	http_410, // Gone
+	http_411, // Length Required
+	http_412, // Precondition Failed
+	http_413, // Payload Too Large
+	http_414, // URI Too Long
+	http_415, // Unsupported Media Type
+	http_416, // Range Not Satisfiable
+	http_417, // Expectation Failed
+	http_418, // I'm a teapot
+	http_421, // Misdirected Request
+	http_422, // Unprocessable Entity
+	http_423, // Locked
+	http_424, // Failed Dependency
+	http_425, // Too Early
+	http_426, // Upgrade Required
+	http_428, // Precondition Required
+	http_429, // Too Many Requests
+	http_431, // Request Header Fields Too Large
+	http_451, // Unavailable For Legal Reasons
+	http_500, // Internal Server Error
+	http_501, // Not Implemented
+	http_502, // Bad Gateway
+	http_503, // Service Unavailable
+	http_504, // Gateway Timeout
+	http_505, // HTTP Version Not Supported
+	http_506, // Variant Also Negotiates
+	http_507, // Insufficient Storage
+	http_508, // Loop Detected
+	http_510, // Not Extended
+	http_511 // Network Authentication Required
 };
 
 } // namespace httpmodel
